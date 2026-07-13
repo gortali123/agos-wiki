11ecba30eda1852ce8c5e2219f77025803bcbbd9
Giulio Ortali <g.ortali@reply.it>
2026-07-13 10:47:27 +0200

    adv

--- develop/macros/materialization/drop_on_full_refresh.sql
+++ develop/macros/materialization/drop_on_full_refresh.sql
@@ -1,6 +1,9 @@
 {% macro drop_on_full_refresh() %}
   {% if execute and flags.FULL_REFRESH and model.unique_id in selected_resources %}
-    {% do log('--full-refresh: drop snapshot ' ~ this, info=true) %}
-    {% do adapter.drop_relation(this) %}
+    {% set relation = adapter.get_relation(database=this.database, schema=this.schema, identifier=this.identifier) %}
+    {% if relation is not none %}
+      {% do log('--full-refresh: drop snapshot ' ~ relation, info=true) %}
+      {% do adapter.drop_relation(relation) %}
+    {% endif %}
   {% endif %}
 {% endmacro %}
