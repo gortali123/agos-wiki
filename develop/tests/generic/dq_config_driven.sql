1baad827e2bc71a9fa273af289e007de841728cc
Giulio Ortali <g.ortali@reply.it>
2026-08-25 16:07:48 +0200

    sq

--- develop/tests/generic/dq_config_driven.sql
+++ develop/tests/generic/dq_config_driven.sql
@@ -1,29 +1,32 @@
-{% test dq_config_driven(model, column_name, store_failures=false) %}
+{% test dq_config_driven(model, severity, store_failures=false) %}
 
-{{ config(store_failures = store_failures) }}
+{{ config(store_failures = store_failures, severity = severity) }}
 
 {%- if not execute -%}
   select null as dq_failures where false
 {%- else -%}
 
 {%- set cfg_rows = dbt_utils.get_query_results_as_dict(
-    "select ds_test_type, ds_severity from " ~ env_var('DBT_DATABASE') ~ ".tech.cfg_dq_test_config"
-    ~ " where ds_modello = '" ~ model.identifier ~ "' and ds_colonna = '" ~ column_name ~ "' and fl_active = 'Y'"
+    "select ds_colonna, ds_test_type from " ~ env_var('DBT_DATABASE') ~ ".tech.cfg_dq_test_config"
+    ~ " where ds_modello = '" ~ model.identifier ~ "' and ds_severity = '" ~ severity ~ "' and fl_active = 'Y'"
 ) -%}
 
-{%- if cfg_rows['DS_TEST_TYPE'] | length == 0 -%}
+{%- if cfg_rows['DS_COLONNA'] | length == 0 -%}
   select null as dq_failures where false
 {%- else -%}
 
-  {%- set test_type = cfg_rows['DS_TEST_TYPE'][0] -%}
-  {%- set severity = cfg_rows['DS_SEVERITY'][0] -%}
-  {{ config(severity = severity) }}
+  {%- set parts = [] -%}
+  {%- for i in range(cfg_rows['DS_COLONNA'] | length) -%}
+    {%- set col = cfg_rows['DS_COLONNA'][i] -%}
+    {%- set test_type = cfg_rows['DS_TEST_TYPE'][i] -%}
+    {%- if test_type == 'is_valid_email' -%}
+      {%- do parts.append(test_is_valid_email(model=model, column_name=col)) -%}
+    {%- else -%}
+      {{ exceptions.raise_compiler_error("dq_config_driven: DS_TEST_TYPE non gestito: " ~ test_type) }}
+    {%- endif -%}
+  {%- endfor -%}
 
-  {%- if test_type == 'is_valid_email' -%}
-    {{ test_is_valid_email(model=model, column_name=column_name) }}
-  {%- else -%}
-    {{ exceptions.raise_compiler_error("dq_config_driven: DS_TEST_TYPE non gestito: " ~ test_type) }}
-  {%- endif -%}
+  {{ parts | join('\nunion all\n') }}
 
 {%- endif -%}
 {%- endif -%}
